1f8f8db449ed9f81cd667497cfd14eb35d6c634d
Florian Krämer <florian.kraemer@kreative-design.net>
2020-10-22 22:29:30 +0200

    Renaming RouteCollector to RouteCollection

--- src/functions.php
+++ src/functions.php
@@ -22,14 +22,14 @@ if (! function_exists('FastRoute\simpleDispatcher')) {
             'routeParser' => RouteParser\Std::class,
             'dataGenerator' => DataGenerator\MarkBased::class,
             'dispatcher' => Dispatcher\MarkBased::class,
-            'routeCollector' => RouteCollector::class,
+            'routeCollector' => RouteCollection::class,
         ];
 
         $routeCollector = new $options['routeCollector'](
             new $options['routeParser'](),
             new $options['dataGenerator']()
         );
-        assert($routeCollector instanceof RouteCollector);
+        assert($routeCollector instanceof RouteCollection);
         $routeDefinitionCallback($routeCollector);
 
         return new $options['dispatcher']($routeCollector->getData());
@@ -44,7 +44,7 @@ if (! function_exists('FastRoute\simpleDispatcher')) {
             'routeParser' => RouteParser\Std::class,
             'dataGenerator' => DataGenerator\MarkBased::class,
             'dispatcher' => Dispatcher\MarkBased::class,
-            'routeCollector' => RouteCollector::class,
+            'routeCollector' => RouteCollection::class,
             'cacheDisabled' => false,
         ];
 
@@ -65,7 +65,7 @@ if (! function_exists('FastRoute\simpleDispatcher')) {
             new $options['routeParser'](),
             new $options['dataGenerator']()
         );
-        assert($routeCollector instanceof RouteCollector);
+        assert($routeCollector instanceof RouteCollection);
         $routeDefinitionCallback($routeCollector);
 
         $dispatchData = $routeCollector->getData();
